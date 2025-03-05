fa079f77b1e9d2d7f08fde8923572ce1d651f3b8
Ranit Parida <ranit0120@gmail.com>
2025-03-05 06:19:25 +0000

    By Ranit

---
+++ fibonacciSeries.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
+
+n = 10
+for i in range(n):
+   print(fibonacci(i))
+
+
+"""Iterative Approach"""
+print("Iterative Approach")
+
+a,b=0,1
+
+for i in range(n):
+    a,b= b,a+b
+    print(b,end='')
