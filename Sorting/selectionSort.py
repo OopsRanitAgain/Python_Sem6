263bb2fc6ae26bef3afee4d70b520a28e4a23035
Ranit Parida <ranit0120@gmail.com>
2025-03-06 10:11:13 +0000

    By Ranit

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -1,20 +1,17 @@
-def selectionSort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_index = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-
-    return arr
-
-def main():
-    data = [64, 25, 12, 22, 11]
-    print("Unsorted array:", data)
-    
-    sorted_data = selectionSort(data)
-    print("Sorted array:", sorted_data)
-
-if __name__ == '__main__':
-    main()
+#selection.py
+import numpy as np
+def selection_sort(data):
+    for i in range (len(data)-1):
+        minpos=1+i
+        for j in range(i+1,len(data)):
+            if data[j]<data[minpos]:
+                minpos=j
+        print(f' '.join(str(v) for v in data))
+        print('___'*(i+1))
+        data[i],data[minpos]=data[minpos],data[i]
+    print(f'sorted data: {data}')
+data=np.random.randint(1,50,15)
+print(data)
+selection_sort(data)
+            
+            
