9aef864c56cf5dd0c348d30f72c407a4ff766b3f
Ranit Parida <ranit0120@gmail.com>
2025-03-06 09:28:44 +0000

    By Ranit

--- binary.py
+++ binary.py
@@ -1,36 +1,40 @@
-def binarySearch(data,key):
-    low=0
-    high = len(data)-1
-    mid = (low+high+1)//2
+import numpy as np
+
+def remainingArray(data,low,high):
+    s ='   '*low
+    return s+' '.join(str(v) for v in data[low:high+1])
+
+def binarySearch(data, key):
+    low = 0
+    high = len(data) - 1
     location = -1
     while low <= high and location == -1:
+        mid = (low + high) // 2
         if key == data[mid]:
             location = mid
         elif key < data[mid]:
-            high = mid-1
+            high = mid - 1
         else:
             low = mid + 1
-        mid = (low+high+1)//2
+        print(remainingArray(data,low,high))
     return location
 
-import numpy as np
-
 def main():
-    data = np.random.randint(11,101,10)
-    print(f'Unsorted Data :{data}')
-
+    data = np.random.randint(11, 101, 10)
+    print(f'Unsorted Data: {data}')
+    
     data.sort()
     print(f'Sorted Data: {data}')
     
-    key = int(input('Enter a Key You want to locate(-1 to quit) :'))
+    key = int(input('Enter a Key You want to locate (-1 to quit): '))
     
     while key != -1:
-        loc = binarySearch(data,key)
+        loc = binarySearch(data, key)
         if loc == -1:
-            print('Key {key} NOT found ')
+            print(f'Key {key} NOT found')
         else:
             print(f'Key is found at location: {loc}')
-        key = int(input('Enter a Key You want to locate(-1 to quit) :'))
+        key = int(input('Enter a Key You want to locate (-1 to quit): '))
 
 if __name__ == '__main__':
     main()
